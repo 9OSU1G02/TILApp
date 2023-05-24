5dff006dbe38edbca518be3ac470d3ccae9ee0b1
9OSU1G02 <huyquocnguyen1902@gmail.com>
2023-05-24 23:30:32 +0700

    User CRUD

---
+++ Sources/App/Migrations/CreateUser.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//
+//
+//  Created by Nguyen Quoc Huy on 5/24/23.
+//
+
+import Fluent
+
+struct CreateUser: Migration {
+    func prepare(on database: FluentKit.Database) -> NIOCore.EventLoopFuture<Void> {
+        database.schema("users").id().field("name", .string, .required).field("username", .string, .required).create()
+    }
+
+    func revert(on database: FluentKit.Database) -> NIOCore.EventLoopFuture<Void> {
+        database.schema("users").delete()
+    }
+}
---
+++ Sources/App/User.swift
@@ -0,0 +1,55 @@
+//
+//  File.swift
+//
+//
+//  Created by Nguyen Quoc Huy on 5/24/23.
+//
+
+import Fluent
+import Vapor
+
+final class User: Model, Content {
+    static let schema = "users"
+    @ID(key: .id)
+    var id: UUID?
+
+    @Field(key: "name")
+    var name: String
+    @Field(key: "username")
+    var username: String
+    init() {}
+    init(id: UUID? = nil, name: String, username: String) {
+        self.id = id
+        self.name = name
+        self.username = username
+    }
+}
+
+final class Student {
+    @Uppercased(wrappedValue: "haha")
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+    init() {
+        print("---> default")
+    }
+}
+
+@propertyWrapper // 1
+struct Uppercased {
+    var value: String // 2
+    
+    var wrappedValue: String {    // 3
+        get {
+            value.uppercased()
+        }
+        set {
+            value = newValue
+        }
+    }
+        // 4
+    init(wrappedValue: String)  {
+        self.value = wrappedValue
+    }
+}
